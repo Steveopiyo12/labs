8173d77ac7304732d676ba0d8a11635f970829e6
Steve Austine Opiyo <steveopiyo12@gmail.com>
2025-05-21 18:35:14 +0300

    Minor Changes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})
+});
 Route::get('/authentication',function(){
     return view('authentication');
+
+});
